58c104579343b5ef624a90cb5239b08da7fbce92
erland-wt <erlandwidyatamaka05@gmail.com>
2026-06-04 14:29:49 +0700

    Initial commit

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,18 +1,32 @@
 import axios from "axios"
 import type { PredictionResponse } from "../types/prediction"
 
-const API_URL = "http://127.0.0.1:8000"
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "http://127.0.0.1:8000"
 
 export const predictSentiment = async (
   text: string
 ): Promise<PredictionResponse> => {
 
-  const response = await axios.post(
-    `${API_URL}/predict`,
-    {
-      text: text
-    }
-  )
+  try {
 
-  return response.data
+    const response = await axios.post(
+      `${API_URL}/predict`,
+      {
+        text
+      }
+    )
+
+    return response.data
+
+  } catch (error) {
+
+    console.error(
+      "Prediction Error:",
+      error
+    )
+
+    throw error
+  }
 }
