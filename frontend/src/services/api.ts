63132114bf564be1e3378042c828f7111c0ad915
erland-wt <erlandwidyatamaka05@gmail.com>
2026-06-04 21:18:14 +0700

    Use deployed backend API

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,26 +2,22 @@ import axios from "axios"
 import type { PredictionResponse } from "../types/prediction"
 
 const API_URL =
-  import.meta.env.VITE_API_URL ||
-  "http://127.0.0.1:8000"
+  import.meta.env.VITE_API_URL
 
 export const predictSentiment = async (
   text: string
 ): Promise<PredictionResponse> => {
 
   try {
-
     const response = await axios.post(
       `${API_URL}/predict`,
       {
         text
       }
     )
-
+    
     return response.data
-
   } catch (error) {
-
     console.error(
       "Prediction Error:",
       error
