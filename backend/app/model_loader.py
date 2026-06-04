a51cabbf8949bd3186ac70f82e926125ce29337e
erland-wt <erlandwidyatamaka05@gmail.com>
2026-06-04 15:52:09 +0700

    Remove binary models from repository

--- backend/app/model_loader.py
+++ backend/app/model_loader.py
@@ -1,19 +1,57 @@
-from pathlib import Path
+import os
+import urllib.request
 import joblib
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+MODEL_DIR = "models"
 
-MODEL_PATH = (
-    BASE_DIR /
-    "models" /
+os.makedirs(
+    MODEL_DIR,
+    exist_ok=True
+)
+
+MODEL_PATH = os.path.join(
+    MODEL_DIR,
     "logistic_regression_model_v1.pkl"
 )
 
-TFIDF_PATH = (
-    BASE_DIR /
-    "models" /
+TFIDF_PATH = os.path.join(
+    MODEL_DIR,
     "tfidf.pkl"
 )
+
+MODEL_URL = (
+    "https://raw.githubusercontent.com/"
+    "erland-wt/"
+    "sentiment-analysis-platform/"
+    "main/"
+    "backend/"
+    "models/"
+    "logistic_regression_model_v1.pkl"
+)
+
+TFIDF_URL = (
+    "https://raw.githubusercontent.com/"
+    "erland-wt/"
+    "sentiment-analysis-platform/"
+    "main/"
+    "backend/"
+    "models/"
+    "tfidf.pkl"
+)
+
+if not os.path.exists(MODEL_PATH):
+    print("Downloading model...")
+    urllib.request.urlretrieve(
+        MODEL_URL,
+        MODEL_PATH
+    )
+
+if not os.path.exists(TFIDF_PATH):
+    print("Downloading TF-IDF...")
+    urllib.request.urlretrieve(
+        TFIDF_URL,
+        TFIDF_PATH
+    )
 
 model = joblib.load(
     MODEL_PATH
